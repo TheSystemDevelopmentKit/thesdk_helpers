e2e6efef11476b7a16f22d95648550c4421a4ecf
Marko Kosunen <marko.kosunen@aalto.fi>
2024-10-06 10:49:27 +0300

    Update development branch initialization in thesdk_helpers

--- shell/init_devel_branch.sh
+++ shell/init_devel_branch.sh
@@ -2,13 +2,13 @@
 #############################################################################
 # Initialize all tracked submodules to the same devel branch as the main
 # project if they do not have that devel branch
-# 
+#
 # Written by Marko Kosunen, marko.kosunen@aalto.fi, 20.11.2022
 #############################################################################
 
 help_f()
 {
-cat << EOF    
+cat << EOF
 init_devel_branch 1.0 (22.11.2022)
 For initialization of TheSyDeKick development branhces
 Written by Marko Pikkis Kosunen
@@ -19,21 +19,21 @@ DESCRIPTION
    Defines and runs tests for the submodules of thesdk_template
 
 OPTIONS
-  -b 
+  -b
      Branch of thesdk_template to create. Defaults to 'master',
      i.e creates master branch for all submodules if it does not exist.
 
-  -c Run in CI/CD with this option. Currently not used 
+  -c Run in CI/CD with this option. Currently not used
 
   -t
-     STRING : Access token, currently not used 
+     STRING : Access token, currently not used
   -h
       Show this help.
 EOF
 }
 
 
-# Token we use for push is given as the first argument
+WORKDIR=$(pwd)
 CICD="0"
 TOKEN=""
 BRANCH="master"
@@ -47,18 +47,7 @@ do
     \?) help_f;;
   esac
 done
-
-if [ -z "${BRANCH}" ]; then
-    echo "Branch not given"
-    exit 1
-fi
-
-#if [ -z "$TOKEN" ]; then
-#    echo "Token must be provided for CI/CD"
-#    exit 1
-#fi
-
-PID="$$"
+# Functions
 function branchtest_local() {
     branch=$1
     # Local branches
@@ -77,53 +66,75 @@ function branchtest_remote() {
 }
 function branchtest() {
     brach=$1
-    if [ -z "$(branchtest_local ${BRANCH})" ]; then 
+    if [ -z "$(branchtest_local ${BRANCH})" ]; then
         echo "$(branchtest_remote ${BRANCH})"
     else
         echo "$(branchtest_local ${BRANCH})"
     fi
-    
+
 }
-#If not in CICD, we will make a test clone.
-if [ "$CICD" != "1" ]; then
-    WORKDIR=$(pwd)
-    if [ -z "$(branchtest ${BRANCH})" ]; then
-        echo "Branch ${BRANCH} does not exits, creating from the current and checkout"
-        git branch ${BRANCH} && git checkout ${BRANCH} || exit 1
-    else
-        echo "Branch ${BRANCH} exits, check out, update and initialize submodules"
-        git checkout "$BRANCH" 
-        if [ ! -z "$(branchtest_remote ${BRANCH})" ]; then
-            git pull || exit 1
-        fi
-        ./init_submodules.sh
-    fi
-else
-    echo "CICD currently not supported"
-    #git config --global --add safe.directory /__w/thesdk_template/thesdk_template
-    #WORKDIR=$(pwd)
+
+# Tests
+if [ ! -d "./Entities" ]; then
+    echo "This script should be called in TheSyDeKick project root."
+    echo " You re in ${WORKDIR}"
+    exit 1
 fi
 
-# Loop through the submodules
-SUBMODULES="$(sed -n '/^SUBMODULES/,/^"/{//!p}' ./init_submodules.sh | xargs)"
+if [ -z "${BRANCH}" ]; then
+    echo "Branch not given"
+    exit 1
+fi
+
+
+ # PID of the current shell
+PID="$$"
+# Handle all teh submodules in a single loop
+MODULES="$WORKDIR"
+MODULES+=" $(sed -n '/^SUBMODULES/,/^"/{//!p}' ./init_submodules.sh | xargs)"
+
+# Logic: Loop through the modules and create development branches if they do not exist
+# Do not push the toplevel, it is handled separately
 UNDERDEVEL=""
-for module in $SUBMODULES; do
+for module in $MODULES; do
     echo "Operating on $module"
     cd $module
-    if [ -z "$(branchtest ${BRANCH})" ]; then
-        echo "Branch ${BRANCH} does not exits, creating from the current and checkout"
-        git branch ${BRANCH} && git checkout ${BRANCH} || exit 1
-    else
-        echo "Branch ${BRANCH} exits, check out and update"
+    if [ ! -z "$(branchtest_remote ${BRANCH})" ]; then
+        echo "Remote branch ${BRANCH} exits, check out, update and initialize submodules"
+        git checkout "$BRANCH"
+        git pull || exit 1
+        if [ -f "./init_submodules.sh" ]; then
+            ./init_submodules.sh
+        fi
+    elif [ ! -z "$(branchtest_local ${BRANCH})" ]; then
+        echo "Local branch ${BRANCH} exits, check out and initialize submodules"
         git checkout "$BRANCH" || exit 1
+        if [ -f "./init_submodules.sh" ]; then
+            ./init_submodules.sh
+        fi
+    else
+        echo "Branch ${BRANCH} does not exits, create and check out, and initialize submodules"
+        git checkout -b "$BRANCH" || exit 1
+        if [ -f "./init_submodules.sh" ]; then
+            ./init_submodules.sh
+        fi
+    fi
+    # Add and push only submodules to UNDERDEVEL
+    if [ "$module" != "${WORKDIR}" ]; then
         if [ ! -z "$(branchtest_remote ${BRANCH})" ]; then
-            git pull || exit 1
-        else
-            echo "Remote branch ${BRANCH} does not exist. Do you wish to push [y|n]"
+            echo "Remote branch ${BRANCH} exits. Do you wish to push? [y|n]"
             read ANS
             if [ "$ANS" == "y" ]; then
-                git push -u origin "${BRANCH}"
                 ANS=""
+                git push
+                UNDERDEVEL="${UNDERDEVEL} ${module}"
+            fi
+        else
+            echo "Push local branch ${BRANCH} to remote. Is that what you wish? [y|n]"
+            read ANS
+            if [ "$ANS" == "y" ]; then
+                ANS=""
+                git push -u origin ${BRANCH}
                 UNDERDEVEL="${UNDERDEVEL} ${module}"
             fi
         fi
@@ -131,9 +142,10 @@ for module in $SUBMODULES; do
     cd ${WORKDIR}
 done
 
+# Stage changes at the toplevel
 if [ ! -z "${UNDERDEVEL}" ]; then
     cd ${WORKDIR}
-    for entity in ${UNDERDEVEL}; do 
+    for entity in ${UNDERDEVEL}; do
         echo "Staging $entity"
         git add "${entity}"
     done
@@ -150,11 +162,6 @@ done)
 EOF
 )"
     echo "$COMMITMESSAGE"
-    #if [ ${CICD} == "1" ]; then 
-    #    git config --global user.name "ecdbot"
-    #    git config --global user.email "${GITHUB_ACTOR}@noreply.github.com"
-    #    git remote set-url origin "https://x-access-token:${TOKEN}@github.com/TheSystemDevelopmentKit/thesdk_template.git"
-    #fi
     echo "Do you wish to commit? [y|n]"
     read ANS
     if [ "$ANS" == "y" ]; then
